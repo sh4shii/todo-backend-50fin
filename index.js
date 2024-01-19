89313b90a11ac4f34e1ad2e650f79530efdbfde0
sh4shii <shashisaurav50@gmail.com>
2024-01-19 13:30:41 +0530

    added cors

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv").config();
 const connectDb = require("./config/dbConnection");
 const userRoutes = require("./routes/userRoutes");
 const todoRoutes = require("./routes/todoRoutes");
+const cors = require('cors');
 
 const port = process.env.PORT || 4000;
 
@@ -12,9 +13,10 @@ const app = express();
 
 // Middleware to parse JSON in the request body
 app.use(express.json());
+app.use(cors());
 
-app.use("/api/user", userRoutes); 
-app.use("/api/todo", todoRoutes); 
+app.use("/api/user", userRoutes);
+app.use("/api/todo", todoRoutes);
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
